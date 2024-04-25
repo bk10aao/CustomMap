d54f1300fe4875eb083d4708defa59b05b2f44d5
Dr-Psycho-Ben <kane_500@hotmail.com>
2024-04-25 13:58:00 +0100

    refactored to use LinkedList for clashing hash values

--- src/main/java/CustomMap.java
+++ src/main/java/CustomMap.java
@@ -1,80 +1,166 @@
-import java.util.Arrays;
 import java.util.LinkedList;
 
 public class CustomMap<T> {
 
-    private T[] map;
+    private LinkedList<MapEntry>[] map;
     private final T key;
     private final T type;
 
-    private int mapSize = 16;
+    private int mapSize;
     private int size = 0;
-    private static double LOAD_FACTOR = 0.75;
+    private static final double LOAD_FACTOR = 0.75;
     private int primesIndex = 0;
 
     public CustomMap(T key, T value) {
-        this.map = (T[]) new Object[mapSize];
+        this.map = new LinkedList[primes[primesIndex]];
+        this.mapSize = primes[0];
+        this.key = key;
+        this.type = value;
+    }
+
+    private CustomMap(T key, T value, int mapSize) {
+        this.map = new LinkedList[mapSize];
+        this.mapSize = mapSize;
         this.key = key;
         this.type = value;
     }
 
     public void clear() {
-        mapSize = 16;
-        size = 0;
-        this.map = (T[]) new Object[mapSize];
+        this.map = new LinkedList[primes[primesIndex]];
+        this.mapSize = primes[0];
+        this.primesIndex = 0;
+        this.size = 0;
     }
 
     public boolean containsValue(T value) {
-        if(value.getClass() == type) return Arrays.stream(map).anyMatch(item -> item != null && item.equals(value));
+        for(LinkedList<MapEntry> entryList : map) {
+            if(entryList != null) {
+                for(MapEntry entry : entryList) {
+                    if(entry.value.equals(value)) {
+                        return true;
+                    }
+                }
+            }
+        }
         return false;
     }
 
     public T get(T key) {
-        return key.getClass() == this.key ? map[Math.abs(key.hashCode()) % mapSize] : null;
+        if(!key.getClass().equals(this.key))
+            return null;
+        int index = Math.abs(key.hashCode()) % mapSize;
+        LinkedList<MapEntry> indexedMapEntry = map[index];
+        if(indexedMapEntry == null)
+            return null;
+        for(MapEntry mapEntry : indexedMapEntry) {
+            if(mapEntry.key.equals(key)) {
+                return mapEntry.value;
+            }
+        }
+        return null;
+    }
+
+    //this method is really jsut for testing LOAD_FACTOR
+    public int getMapSize() {
+        return this.mapSize;
     }
 
     public T put(T key, T value) {
-        T previous = null;
-        if(key.getClass() == this.key && value.getClass() == this.type) {
-            int index = Math.abs(key.hashCode()) % mapSize;
-            previous = map[index];
-            if (previous == null) {
-                size++;
-            }
-            map[index] = value;
+        if(!key.getClass().equals(this.key) || !value.getClass().equals(this.type)) {
+            throw new IllegalArgumentException();
         }
-        if((double)size / (double)mapSize > LOAD_FACTOR)
-            expand();
-        return previous;
+        int index = Math.abs(key.hashCode()) % mapSize;
+        LinkedList<MapEntry> indexedEntry = map[index];
+        if(indexedEntry == null) {
+            addNewIndexEntry(key, value, index);
+            if((double)size / (double)mapSize > LOAD_FACTOR)
+                expand();
+            return null;
+        } else {
+            T result = updateExistingLinkedList(index, key, value);
+            double currentLoad = (double)size / (double)mapSize;
+            if(currentLoad > LOAD_FACTOR) {
+                expand();
+            }
+            return result;
+        }
     }
 
     public T remove(T key) {
-        T item = null;
-        if(key.getClass() == this.key) {
-            int index = Math.abs(key.hashCode()) % mapSize;
-            item = map[index];
-            if (item != null) {
-                size--;
-                map[index] = null;
+        int index = Math.abs(key.hashCode()) % mapSize;
+        LinkedList<MapEntry> entryLinkedList = map[index];
+        if(get(key) != null) {
+            System.out.println(1);
+        }
+        if(entryLinkedList != null) {
+            for(int i = 0; i < entryLinkedList.size(); i++) {
+                MapEntry entry = entryLinkedList.get(i);
+                if(entry.key.equals(key)) {
+                    MapEntry removed = entryLinkedList.remove(i);
+                    size--;
+                    System.out.println("map size: " + mapSize + " entries " + size);// + " current size: " + size + " Load factored: " + mapSize / 4);
+                    if(mapSize > 17 && size <= mapSize / 4)
+                        reduce();
+                    return removed.value;
+                }
             }
         }
-        return item;
+        return null;
     }
 
     public int size() {
-        return size;
+        return this.size;
+    }
+
+    private void addNewIndexEntry(T key, T value, int index) {
+        map[index] = new LinkedList<>();
+        LinkedList<MapEntry> entryIndex = map[index];
+        MapEntry mapEntry = new MapEntry(key, value);
+        entryIndex.add(mapEntry);
+        map[index] = entryIndex;
+        size++;
     }
 
     private void expand() {
         mapSize = primes[++primesIndex];
-        T[] newMap = (T[]) new Object[mapSize];
-
-        for (T item : map)
-            if (item != null) {
-                int index = Math.abs(key.hashCode()) % mapSize;
-                newMap[index] = item;
+        CustomMap<T> newMap = new CustomMap(this.key, this.type, mapSize);
+        for (LinkedList<MapEntry> linkedList : map) {
+            if (linkedList != null) {
+                for(MapEntry entry : linkedList) {
+                    newMap.put(entry.key, entry.value);
+                }
             }
-        map = newMap;
+        }
+        map = newMap.map;
+    }
+
+    private void reduce() {
+        primesIndex = (primesIndex / 2);
+        mapSize = primes[primesIndex];
+        CustomMap newMap = new CustomMap(this.key, this.type, mapSize);
+
+        for (int i = 0; i < map.length; i++) {
+            if (map[i] != null)
+                for (MapEntry item : map[i]) {
+                    newMap.put(item.key, item.value);
+                }
+        }
+        map = newMap.map;
+    }
+
+    private T updateExistingLinkedList(int index, T key, T value) {
+
+        LinkedList<MapEntry> currentEntries = map[index];
+        for(int i = 0; i < currentEntries.size(); i++) {
+            if (currentEntries.get(i).key.equals(key)) {
+                MapEntry previous = currentEntries.get(i);
+                currentEntries.get(i).value = value;
+                return previous.value;
+            }
+        }
+        map[index].add(new MapEntry(key, value));
+        size++;
+        return null;
     }
 
     protected static final int[] primes = { 17, 23, 29, 37, 47, 59, 71, 89, 107, 131, 163, 197, 239, 293, 353, 431, 521, 631, 761, 919,
@@ -82,4 +168,36 @@ public class CustomMap<T> {
             17519, 21023, 25229, 30293, 36353, 43627, 52361, 62851, 75431, 90523, 108631, 130363, 156437,
             187751, 225307, 270371, 324449, 389357, 467237, 560689, 672827, 807403, 968897, 1162687, 1395263,
             1674319, 2009191, 2411033, 2893249, 3471899, 4166287, 4999559, 5999471, 7199369 };
+
+    public class MapEntry {
+        T key;
+        T value;
+
+        MapEntry(T key, T value) {
+            this.key = key;
+            this.value = value;
+        }
+    }
 }
+//
+//    public T put(T key, T value) {
+//        if(!key.getClass().equals(this.key) || !value.getClass().equals(this.type)) {
+//            throw new IllegalArgumentException();
+//        }
+//        int index = Math.abs(key.hashCode()) % mapSize;
+//        LinkedList<MapEntry> indexedEntry = map[index];
+//        if(indexedEntry == null) {
+//            addNewIndexEntry(key, value, index);
+//            if((double)size / (double)mapSize > LOAD_FACTOR)
+//                expand();
+//            return null;
+//        } else {
+//            T result = updateExistingLinkedList(index, key, value);
+//            double currentLoad = (double)size / (double)mapSize;
+//            if(currentLoad > LOAD_FACTOR) {
+//                expand();
+//            }
+//            return result;
+//        }
+//
+//    }
--- src/test/java/CustomMapTest.java
+++ src/test/java/CustomMapTest.java
@@ -2,7 +2,10 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class CustomMapTest {
@@ -21,9 +24,15 @@ class CustomMapTest {
     }
 
     @Test
-    public void createMap_withNoValues_onGetKey_abc_returns_null() {
+    public void createMap_andAddTwoItems_ofSameValue_returnsSizeOf_1() {
         CustomMap map = new CustomMap(String.class, String.class);
-        assertNull(map.get("abc"));
+        assertNull(map.put("abc", "def"));
+        assertEquals(1, map.size());
+        Object previous = map.put("abc", "def");
+        assertNotNull(previous);
+
+        assertEquals("def", previous);
+        assertEquals(1, map.size());
     }
 
     @Test
@@ -32,6 +41,12 @@ class CustomMapTest {
         assertNull(map.put("abc", "def"));
         assertNull(map.put("def", "ghi"));
         assertEquals(2, map.size());
+    }
+
+    @Test
+    public void createMap_withNoValues_onGetKey_abc_returns_null() {
+        CustomMap map = new CustomMap(String.class, String.class);
+        assertNull(map.get("abc"));
     }
 
     @Test
@@ -62,14 +77,24 @@ class CustomMapTest {
     }
 
     @Test
+    public void onCreatingMapOfKeyValue_String_String_onAddingEntryOf_Integer_Integer_throw_IllegalArgumentException() {
+        CustomMap map = new CustomMap(String.class, String.class);
+        assertNull(map.put("abc", "def"));
+        assertEquals(1, map.size());
+        assertThrows(IllegalArgumentException.class, ()-> map.put(10, 10));
+    }
+
+    @Test
     public void createMap_addKeyValue_123_456_on_put_123_789_returnsPreviousValue_456_andUpdatesKey_123_to_789() {
         CustomMap map = new CustomMap(String.class, Integer.class);
         assertNull(map.put("123", 456));
         Object value = map.get("123");
-        assertEquals(456, value);
-        assertEquals(456, map.put("123", 789));
-        value = map.get("123");
-        assertEquals(789, value);
+        assertEquals(value, 456);
+
+        Object updatedValue = map.put("123", 789);
+        assertNotNull(updatedValue);
+        assertEquals(789, updatedValue);
+        assertNotEquals(value, updatedValue);
     }
 
     @Test
@@ -128,18 +153,38 @@ class CustomMapTest {
     }
 
     @Test
-    public void onAdding_12Items_withLoadFactorOf_point_75_returns_MapSizeOf_17_andCorrectlyRehashedValues() {
-        CustomMap map = new CustomMap(String.class, Integer.class);
-        assertNull(map.put("1", 1));
-        assertNull(map.put("2", 2));
-        assertNull(map.put("3", 3));
-        assertNull(map.put("4", 4));
-        assertNull(map.put("5", 5));
-        assertNull(map.put("6", 6));
-        assertNull(map.put("7", 7));
-        assertNull(map.put("8", 8));
-        assertNull(map.put("9", 9));
-        assertNull(map.put("10", 10));
-        assertNull(map.put("11", 11));
+    public void onAdding_13Items_withLoadFactorOf_point_75_returns_MapSizeOf_23_andCorrectlyRehashedValues() {
+        CustomMap map = new CustomMap(Integer.class, Integer.class);
+        assertEquals(17, map.getMapSize());
+        for (int i = 0; i < 13; i++) {
+            map.put(i, i * 10);
+        }
+        assertEquals(23, map.getMapSize());
+        for(int i = 0; i < 7; i++) {
+            assertEquals(i * 10, map.get(i));
+        }
+    }
+
+    @Test
+    public void onAdding_13Items_withLoadFactorOf_point_75_returns_MapSizeOf_23_andOnReducingMapToSizeOf_5_reducesMapSizeTo_17() {
+        CustomMap map = new CustomMap(Integer.class, Integer.class);
+        assertEquals(17, map.getMapSize());
+        for(int i = 1; i < 14; i++) {
+            map.put(i, i * 10);
+        }
+        assertEquals(23, map.getMapSize());
+        assertEquals(13, map.size());
+
+        for(int i = 1; i < 9; i++) {
+            map.remove(i);
+        }
+
+        assertEquals(5, map.size());
+        assertEquals(17, map.getMapSize());
+
+        for(int i = 10; i < 14; i++) {
+            assertEquals(i * 10, map.get(i));
+        }
+        System.out.println(map.size());
     }
 }
