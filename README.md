5e9733a3d28cdd7fa1906197c8fb812ee5b626c9
Dr-Psycho-Ben <kane_500@hotmail.com>
2024-11-08 12:19:48 +0000

    updating to use <K, V> instead of <T, T>

--- README.md
+++ README.md
@@ -2,21 +2,21 @@
 Implementation of a Map using an array
 
 # Methods
-1. `CustomMap(T key, T value)` - constructor.
-2. `CustomMap(T key, T value, T mapSize)` - constructor.
+1. `CustomMap(K key, V value)` - constructor.
+2. `CustomMap(K key, V value, int mapSize)` - constructor.
 3. `void clear()` - resets map to empty.
-4. `boolean containsKey(T key)` - returns true if contains key, else false. Throws NullPointerException on null key.
-5. `boolean containsValue(T value)` - returns true if contains value, else false. Throws NullPointerException on null value.
-6. `boolean equals(Object map)` - returns true if the map matches, else false.
-7. `T get(T key)` - returns object associated to key, else null if key not present. Throws NullPointerException on null key. 
-8. `T getOrDefault(T key, T defaultValue)` - get value associated to key if present, else return default value. Throws NullPointerException on null key.
+4. `boolean containsKey(K key)` - returns true if contains key, else false. Throws NullPointerException on null key.
+5. `boolean containsValue(V value)` - returns true if contains value, else false. Throws NullPointerException on null value.
+6. `boolean equals(CustomMap<K, V> o)` - returns true if the map matches, else false.
+7. `T get(K key)` - returns value associated to key, else null if key not present. Throws NullPointerException on null key. 
+8. `T getOrDefault(K key, V defaultValue)` - get value associated to key if present, else return default value. Throws NullPointerException on null key.
 9. `Set keySet()` - returns Set of keys in map.
-10. `T put(T key, T value)` - puts value into map with associated key. Returns item previous associated with key if present, else null. Throws IllegalArgumentException on non-matching key type or value type. Throws NullPointerException if key or value is null.
-11. `T putIfAbsent(T key, T value)` - puts value in map if not present, returns previous associated item if present else null. Throws NullPointerException on null key or null value.
-12. `T remove(T key)` - remove and return item from map if present else returns null. Throws NullPointerException on null key.
-13. `boolean remove(T key, T value)` - removes value from map if key is present and matches value. Throws NullPointerException on null key or value.
-14. `T replace(T key, T value)` - replace the value for associated key. Returns item previously associated to key, else false. throws NullPointerException on null key or value.
-15. `boolean replace(T key, T oldValue, T newValue)` - replaces value in map if associated value matches previous value. Returns true if successful. Throws NullPointerException on null key, oldValue or newValue.
+10. `V put(K key, V value)` - puts value into map with associated key. Returns item previously associated with key if present, else null. Throws IllegalArgumentException on non-matching key type or value type. Throws NullPointerException if key or value is null.
+11. `V putIfAbsent(K key, V value)` - puts value in map if not present, returns previous associated item if present else null. Throws NullPointerException on null key or null value.
+12. `V remove(K key)` - remove and return value from map if present else returns null. Throws NullPointerException on null key.
+13. `boolean remove(K key, V value)` - removes value from map if key is present and matches value. Throws NullPointerException on null key or value.
+14. `V replace(K key, V value)` - replace the value for associated key. Returns item previously associated to key, else false. throws NullPointerException on null key or value.
+15. `boolean replace(K key, V oldValue, V newValue)` - replaces value in map if associated value matches previous value. Returns true if successful, else false. Throws NullPointerException on null key, oldValue or newValue.
 16. `int size()` - returns number of items in map.
 17. `String toString()` - returns string representation of map.
-18. `Collection<T> values()` - returns a collection of all values associated to keys in map.
+18. `Collection<V> values()` - returns a collection of all values associated to keys in map.
--- src/main/java/MapInterface.java
+++ src/main/java/MapInterface.java
@@ -1,7 +1,7 @@
 import java.util.Collection;
 import java.util.Set;
 
-public interface MapInterface<T> {
+public interface MapInterface<K, V> {
 
     /**
      * Clear all items from Map.
@@ -10,43 +10,43 @@ public interface MapInterface<T> {
 
     /**
      * Check if Map contains Key
-     * @param key - item to be searched for in Map.
+     * @param key - key for value to be searched for in Map.
      * @return boolean true if contains key, else false.
      * @throws NullPointerException on null key.
      */
-    boolean containsKey(T key);
+    boolean containsKey(K key);
 
     /**
      * Check if Map contains Value
-     * @param value - item to be searched for in Map.
+     * @param value - value to be searched for in Map.
      * @return boolean true if contains value, else false.
-     * @throws NullPointerException on null item.
+     * @throws NullPointerException on null value.
      */
-    boolean containsValue(T value);
+    boolean containsValue(V value);
 
     /**
      * Check if Map is equal to provided map
      * @param map - map to be compared to.
      * @return boolean true if equals, else false.
      */
-     boolean equals(Object map);
+     boolean equals(CustomMap<K, V> map);
 
     /**
-     * Get Object by key.
-     * @param key - key of item to be returned.
+     * Get Value by key.
+     * @param key - key of value to be returned.
      * @return value associated to key, else null.
      * @throws NullPointerException on null key.
      */
-    T get(T key);
+    V get(K key);
 
     /**
-     * Get Object by key.
-     * @param key - key of item to be returned.
+     * Get Value by key.
+     * @param key - key of value to be returned.
      * @param defaultValue - value to be returned if not present
-     * @return value associated to key.
+     * @return value associated to key, or default value.
      * @throws NullPointerException on null key.
      */
-    T getOrDefault(T key, T defaultValue);
+    V getOrDefault(K key, V defaultValue);
 
     /**
      * Get keys as Set.
@@ -55,32 +55,32 @@ public interface MapInterface<T> {
     Set keySet();
 
     /**
-     * Put item in map
+     * Put value in map
      * @param key - key to map to value
      * @param value - value to be associated to key
      * @throws IllegalArgumentException on non-matching key type or value type
      * @throws NullPointerException on null key
-     * @return T item for previous key, else null if not previously used
+     * @return V item for previous key, else null if not previously used
      */
-    T put(final T key, final T value);
+    V put(final K key, final V value);
 
     /**
-     * Put item in map if not previously present, else returns previous value
+     * Put value in map if not previously present, else returns previous value
      * @param key - key to map to value
      * @param value - value to be associated to key
      * @throws NullPointerException on null key
      * @throws NullPointerException on null value
-     * @return T item for previous key, else null if not previously used
+     * @return V item for previous key, else null if not previously used
      */
-    T putIfAbsent(final T key, final T value);
+    V putIfAbsent(final K key, final V value);
 
     /**
      * Remove item from map if key exist
      * @param key - key of item to remove
      * @throws NullPointerException on null key
-     * @return T item for previous key, else null if not previously used
+     * @return V item for previous key, else null if not previously used
      */
-    T remove(final T key);
+    V remove(final K key);
 
     /**
      * Remove item from map if key and exists and value matches what is associated to key
@@ -90,7 +90,7 @@ public interface MapInterface<T> {
      * @throws NullPointerException on null value
      * @return true if removed, else false;
      */
-    boolean remove(final T key, final T value);
+    boolean remove(final K key, final V value);
 
     /**
      * Replace value if key exists in Map
@@ -98,9 +98,9 @@ public interface MapInterface<T> {
      * @param value - new value to be set
      * @throws NullPointerException on null key
      * @throws NullPointerException on null old value
-     * @return T item previously associated to key, else null
+     * @return V item previously associated to key, else null
      */
-    T replace(T key, T value);
+    V replace(K key, V value);
 
     /**
      * Replace value if key exists in Map
@@ -112,7 +112,7 @@ public interface MapInterface<T> {
      * @throws NullPointerException on null new value
      * @return true if value is replaced, else false
      */
-     boolean replace(T key, T oldValue, T newValue);
+     boolean replace(K key, V oldValue, V newValue);
 
     /**
      * Get number of items in Map
@@ -130,5 +130,5 @@ public interface MapInterface<T> {
      * Get all values associated to all keys in Map
      * @return Collection of all values associated to all keys in Map
      */
-    Collection<T> values();
+    Collection<V> values();
 }
